b016c9cc90454fb660a0f432dc7f31d790292c9b
Carlos Philipe Mendes Bahia <carlosphilipe@Carlos-Philipe-Mac.local>
2015-05-09 12:56:54 -0400

    init commit

---
+++ TryMapKit/TryMapKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TryMapKit
+//
+//  Created by Carlos Philipe Mendes Bahia on 09/05/15.
+//  Copyright (c) 2015 Carlos Philipe Mendes Bahia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
