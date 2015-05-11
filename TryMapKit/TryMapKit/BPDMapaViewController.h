31c0e41fbb68f04f3d6fd6f6631fe1d8f54bbe88
Carlos Philipe Mendes Bahia <carlosphilipe@Carlos-Philipe-Mac.local>
2015-05-11 08:33:00 -0400

    mapa funcional

--- TryMapKit/TryMapKit/BPDMapaViewController.h
+++ TryMapKit/TryMapKit/BPDMapaViewController.h
@@ -1,19 +1,19 @@
 //
-//  ViewController.h
+//  BPDMapaViewController.h
 //  TryMapKit
 //
-//  Created by Carlos Philipe Mendes Bahia on 09/05/15.
+//  Created by Carlos Philipe Mendes Bahia on 11/05/15.
 //  Copyright (c) 2015 Carlos Philipe Mendes Bahia. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController{
+@interface BPDMapaViewController : UIViewController{
     MKMapView * mapView;
 }
 
 @property(strong, nonatomic) IBOutlet MKMapView * mapView;
 
-@end
 
+@end
---
+++ TryMapKit/TryMapKit/BPDMapaViewController.m
@@ -0,0 +1,39 @@
+//
+//  BPDMapaViewController.m
+//  TryMapKit
+//
+//  Created by Carlos Philipe Mendes Bahia on 11/05/15.
+//  Copyright (c) 2015 Carlos Philipe Mendes Bahia. All rights reserved.
+//
+
+#import "BPDMapaViewController.h"
+
+@interface BPDMapaViewController ()
+
+@end
+
+@implementation BPDMapaViewController
+
+@synthesize mapView = _mapview;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
